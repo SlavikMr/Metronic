d3e292e0fd5991ed9dc342a2c1332fb775de83ee
Slavik <slavik.m992@gmail.com>
2018-01-17 22:26:59 +0200

    FullSection

--- src/apps/Components/Buttons/DefaultStyle.js
+++ src/apps/Components/Buttons/DefaultStyle.js
@@ -1,38 +1,100 @@
 import React from 'react';
 import { Button } from 'common/components/Button';
 import { ButtonGroup } from 'reactstrap';
+import { Portlet } from 'common/components/Portlet';
+import PropTypes from 'prop-types';
+
+const longSubTitle = (
+  <React.Fragment>
+    The <code>.btn</code> classes are designed to be used with
+    <code>&lt;button&gt;</code>, <code>&lt;a&gt;</code>,
+    and <code>&lt;input&gt;</code>elements.
+  </React.Fragment>);
+
+function Section(props) {
+  return <div className="m-section">{props.children}</div>;
+}
+
+function SectionContent(props) {
+  const { subTitle } = props;
+
+  return (
+    <React.Fragment>
+      <span className="m-section__sub">{subTitle}</span>
+      <div className="m-section__content">
+        <div className="m-demo">
+          <div className="m-demo__preview  m-demo__preview--btn">
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Section.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+SectionContent.propTypes = {
+  subTitle: PropTypes.node,
+  children: PropTypes.node.isRequired,
+};
+
+SectionContent.defaultProps = {
+  subTitle: '',
+};
 
 const DefaultStyle = () => (
   <div>
-    <div>
-      <Button color="primary">Primary</Button>
-      <Button color="secondary">Secondary</Button>
-      <Button color="success">Success</Button>
-      <Button color="info">Info</Button>
-      <Button color="warning">Warning</Button>
-      <Button color="danger">Danger</Button>
-      <Button color="link">Link</Button>
-    </div>
-    <div>
-      <Button tag="button" color="brand">Brand</Button>
-      <Button tag="button" color="metal">Metal</Button>
-      <Button tag="button" color="accent">Accent</Button>
-      <Button tag="button" color="focus">Focus</Button>
-    </div>
-    <div>
-      <Button tag="a" color="primary" href="">Link</Button>
-      <Button tag="button" color="success">Button</Button>
-      <Button tag="input" type="button" color="warning" value="Input" />
-      <Button tag="input" type="submit" color="info" value="Submit" />
-      <Button tag="input" type="reset" color="danger" value="Reset" />
-      <Button tag="button" type="reset" color="brand">Brand</Button>
-    </div>
-    <div>
-      <Button tag="button" color="primary" wide>Primary</Button>
-      <Button tag="button" color="secondary" wide>Secondary</Button>
-      <Button tag="button" color="success" wide>Success</Button>
-      <Button tag="button" color="info" wide>Info</Button>
-      <Button tag="button" color="warning" wide>Warning</Button>
+    <div className="row">
+      <div className="col-lg-6">
+        <Portlet>
+          <Portlet.Header>
+            <Portlet.HeaderCaption title="Base Buttons" />
+          </Portlet.Header>
+          <Portlet.Body>
+            <Section>
+
+              <SectionContent>
+                <Button color="primary">Primary</Button>
+                <Button color="secondary">Secondary</Button>
+                <Button color="success">Success</Button>
+                <Button color="info">Info</Button>
+                <Button color="warning">Warning</Button>
+                <Button color="danger">Danger</Button>
+                <Button color="link">Link</Button>
+              </SectionContent>
+
+              <SectionContent subTitle="Metronic custom state buttons">
+                <Button tag="button" color="brand">Brand</Button>
+                <Button tag="button" color="metal">Metal</Button>
+                <Button tag="button" color="accent">Accent</Button>
+                <Button tag="button" color="focus">Focus</Button>
+              </SectionContent>
+
+              <SectionContent subTitle={longSubTitle}>
+                <Button tag="a" color="primary" href="">Link</Button>
+                <Button tag="button" color="success">Button</Button>
+                <Button tag="input" type="button" color="warning" value="Input" />
+                <Button tag="input" type="submit" color="info" value="Submit" />
+                <Button tag="input" type="reset" color="danger" value="Reset" />
+                <Button tag="a" color="metal" href="">Metal</Button>
+                <Button tag="button" type="reset" color="brand">Brand</Button>
+              </SectionContent>
+
+              <SectionContent subTitle="Wide bootstrap buttons">
+                <Button tag="button" color="primary" wide>Primary</Button>
+                <Button tag="button" color="secondary" wide>Secondary</Button>
+                <Button tag="button" color="success" wide>Success</Button>
+                <Button tag="button" color="info" wide>Info</Button>
+                <Button tag="button" color="warning" wide>Warning</Button>
+              </SectionContent>
+
+            </Section>
+          </Portlet.Body>
+        </Portlet>
+      </div>
     </div>
     <div>
       <Button color="primary" size="lg">Large button</Button>
@@ -50,7 +112,7 @@ const DefaultStyle = () => (
       <Button color="secondary" size="sm">Small button</Button>
       <Button outline color="success" size="sm">Small button</Button>
       <Button outline color="info" size="sm">Small button</Button>
-      <Button outline color="brand "size="sm">Small button</Button>
+      <Button outline color="brand " size="sm">Small button</Button>
     </div>
     <div>
       <Button color="primary" size="lg" mBtn mBtnCust>Large button</Button>
